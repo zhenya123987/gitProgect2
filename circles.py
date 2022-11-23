3d9c7bfc2b871880d5c60f7ade014a7738b58189
007 <zhenyatopychkanov@yandex.ru>
2022-11-23 18:58:22 +0500

    fifth commit message

--- circles.py
+++ circles.py
@@ -36,4 +36,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
-    sys.exit(app.exec())  # second third fourth
+    sys.exit(app.exec())  # second third fourth fifth
